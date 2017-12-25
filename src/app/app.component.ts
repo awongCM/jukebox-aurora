0e64a6966ebb3bbaccb5338fc1b28903d2fdad25
awongCM <awong.cm@gmail.com>
2017-12-25 17:56:25 +1100

    added todos api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // TODO
+  private apiURL = 'https://some-api-url/';
+  data: any = {};
+
+  constructor(private http: Http) {
+    console.log('Hello fellow user');
+    this.getContacts();
+  }
+
+  /**
+   * name
+   */
+  public getContacts() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
+
+  public getData() {
+    return this.http.get(this.apiURL)
+      .map((res: Response) => res.json());
+  }
 }
