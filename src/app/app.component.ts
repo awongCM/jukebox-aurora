373fad2561daef47ad36f2c84003f045820beb3e
awongCM <awong.cm@gmail.com>
2017-12-31 14:10:38 +1100

    added login and logout buttons for spotify authorization request; add css layout for buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,19 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   title = 'JukeBox Aurora App';
+  
   // TODO - to put this into services/interface
   client_id = '976b920abb9946b987f1dfe7e95c1942';
   client_secret = '049a3cf08f10408183111691d56fd6c0';
   redirect_uri = 'http://localhost:4200';
+  
+  // LocalStorage
+  state_key = 'spotity_auth_state';
 
   // TODO
   public accessToken: string;
   public tokenType: string;
+
   public tracks: any[];
   public albums: any[];
   data: any = {};
@@ -28,23 +33,42 @@ export class AppComponent {
 
     const access_token = hashParams.access_token,
           token_type = hashParams.token_type,
-          state = hashParams.state;
+          state = hashParams.state,
+          storedState = localStorage.getItem(this.state_key);
 
-    if (!access_token) {
-      this.requestAuthorization();
+    if (access_token && (state == null || state !== storedState)) {
+      alert('Authentication Error detected');
     } else {
-      console.log('access token found!', access_token);
-      console.log('token type found!', token_type);
-      console.log('state found!', state);
+      localStorage.removeItem(this.state_key);
+      if (access_token) {
+        console.log('access token found!', access_token);
+        console.log('token type found!', token_type);
+        console.log('state found!', state);
 
-      // remove hash
-      window.location.hash = '';
+        // remove hash
+        window.location.hash = '';
 
-      this.accessToken = access_token;
-      this.tokenType = token_type;
+        this.accessToken = access_token;
+        this.tokenType = token_type;
 
-      this.getUserTracks();
+        this.getUserTracks();
+      }
     }
+  }
+
+  /**
+   * login Spotify Account
+   */
+  login() {
+    this.requestAuthorization();
+  }
+
+  /**
+   * logout Spotify Account
+   */
+  logout() {
+    this.endAuthorizationRequest();
+    window.location.reload();
   }
 
   /**
@@ -55,7 +79,7 @@ export class AppComponent {
       console.log(data.items);
       this.data = data;
 
-      let tracks = data.items.map( (item) => {return item.track});
+      let tracks = data.items.map( (item) => item.track);
       console.log('tracks', tracks);
       this.tracks = tracks;
     });
@@ -74,16 +98,38 @@ export class AppComponent {
     return hashParams;
   }
 
+  private _generateRandomString(length) {
+    let random_str = '';
+    const combinations = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+      random_str += combinations.charAt(Math.floor(Math.random() * combinations.length));
+    }
+    return random_str;
+  }
+
   /**
    * requestAuthorization using implicit grant access only
    */
   public requestAuthorization() {
     let authorizationTokenUrl = 'https://accounts.spotify.com/authorize';
+
+    let state_value = this._generateRandomString(16);
+    localStorage.setItem(this.state_key, state_value);
+
     let query_params =  [ '?response_type=token', `client_id=${this.client_id}`,
                           'scope=user-library-read', `redirect_uri=${this.redirect_uri}`,
+                          `state=${state_value}`
                         ].join('&');
 
     window.location = authorizationTokenUrl + query_params;
+  }
+
+  /**
+   * endAuthorizationRequest
+   */
+  public endAuthorizationRequest() {
+    this.accessToken = null;
   }
 
   public getData(api_url) {
