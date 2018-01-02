0fb232c1eb71dfb528a06e342a8d0cb98aa88a54
awongCM <awong.cm@gmail.com>
2018-01-02 15:17:24 +1100

    implement jukebox playback functonality when switching different tracks;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { SpotifyAPIService } from './services/spotify-api.service';
 })
 export class AppComponent implements OnInit {
   title = 'JukeBox Aurora App';
-
   private tracks: any[];
+
+  // jukebox properties
+  selected_track = null;
+  isPlaying = false;
+  audio = null;
+  disc = null;
 
   // gallery properties
   readonly THRESHOLD: Number = 0.6;
@@ -68,6 +73,62 @@ export class AppComponent implements OnInit {
 		} else if (fullScreen.msRequestFullscreen) {
       fullScreen.msRequestFullscreen();
 		}
+  }
+
+  track_select(track) {
+    console.log('track selected', track );
+    this.selected_track = track;
+    this.isPlaying = false;
+
+    // reset webkit animations when switching tracks
+    this.disc = document.getElementById('disc');
+    console.log('disc ob', this.disc);
+    this.disc.style.webkitAnimation = 'none';
+    setTimeout(function() {
+      this.disc.style.webkitAnimation = '';
+  }, 10);
+  }
+
+  display_album_artwork() {
+    return (this.selected_track !== null) ? this.selected_track.album.images[0].url : 'http://via.placeholder.com/250x250';
+  }
+
+  display_album_title() {
+    return (this.selected_track !== null) ? this.selected_track.name : 'No audio track is chosen';
+  }
+
+  display_album_url() {
+    return (this.selected_track !== null) ? this.selected_track.preview_url : '';
+  }
+
+  pause_play_track(event) {
+    event.preventDefault();
+
+    this.isPlaying = !this.isPlaying;
+
+    if (this.audio === null) {
+      this.audio = document.getElementById('player_audio');
+      this.audio.src = this.display_album_url();
+      this.audio.load();
+    }
+
+    console.log('audio obj', this.audio);
+
+    if (this.isPlaying) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
+
+  }
+
+  pause_play_state() {
+    return (this.isPlaying) ? 'running' : 'paused';
+  }
+
+  reset_play_state() {
+    console.log('audio ended');
+    this.isPlaying = false;
   }
 
   // TODOS
