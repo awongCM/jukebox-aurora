28089a074d4f95db20f125698cb8e7e3d58726d6
awongCM <awong.cm@gmail.com>
2018-07-05 18:09:41 +1000

    change angular cli to use scss instead of css; upgraded node to use angular v6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { DocumentInterface } from './browsers/document-interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'JukeBox Aurora App';
